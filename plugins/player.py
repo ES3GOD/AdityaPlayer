6c95e5ab9cb1cbdd60a1c409713ec02e3384e0bd
~ES3GOD <89799912+ES3GOD@users.noreply.github.com>
2022-07-08 13:27:32 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -16,7 +16,7 @@ from modules.helpers.admins import get_administrators
 from youtube_search import YoutubeSearch
 from modules import converter
 from modules.downloaders import youtube
-from modules.config import DURATION_LIMIT, que, SUDO_USERS
+from modules.config import DURATION_LIMIT, que, SUDO_USERS, BOT_NAME
 from modules.cache.admins import admins as a
 from modules.helpers.filters import command, other_filters
 from modules.helpers.command import commandpro
